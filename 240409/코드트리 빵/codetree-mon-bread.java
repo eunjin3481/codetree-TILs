56df8bbd0c63e1d4d351bcc3f74b7c09ae015c8c
EunJin Shin <111435676+eunjin3481@users.noreply.github.com>
2024-04-09 00:09:27 +0900

    Title: 코드트리 빵, Time: 2025ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

--- 240409/코드트리 빵/codetree-mon-bread.java
+++ 240409/코드트리 빵/codetree-mon-bread.java
@@ -65,7 +65,7 @@ public class Main {
             // 해당 시간의 사람 베이스 캠프로 이동
             if (time <= m) {
                 for(int i = 0; i < n; i++) {
-                    Arrays.fill(isVisit,false);
+                    Arrays.fill(isVisit[i],false);
                 }
                 goBaseCamp(time);
             }
